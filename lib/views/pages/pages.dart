372b8e3abfefbc7e45173b8d38d7c568447eef6d
michaellchandra <76256177+michaellchandra@users.noreply.github.com>
2022-12-12 21:29:24 +0700

    Creating Register Dart

--- lib/views/pages/pages.dart
+++ lib/views/pages/pages.dart
@@ -2,3 +2,4 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 part 'login.dart';
+part 'register.dart';
---
+++ lib/views/pages/register.dart
@@ -0,0 +1,17 @@
+part of 'pages.dart';
+
+class Register extends StatefulWidget {
+  const Register({ Key? key }) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
