9aa7739d232cada01f36d934b144b2c8f1469d4b
jverdianto <73513762+jverdianto@users.noreply.github.com>
2022-12-14 17:29:56 +0700

    register page added

--- lib/views/pages/register.dart
+++ lib/views/pages/register.dart
@@ -8,10 +8,166 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  bool isHide = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomPaint(
+        painter: BluePainter(),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              padding: EdgeInsets.all(24),
+              child: Align(
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Spacer(),
+                      Text(
+                        "Salam Kenal!",
+                        style: TextStyle(
+                          fontSize: 15, 
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Silahkan Registrasi",
+                        style: TextStyle(
+                          fontSize: 28, 
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 55),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                          labelText: "Nama Lengkap",
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          prefixIcon: Icon(Icons.mail),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                          labelText: "Nomor Telepon",
+                          prefixIcon: Icon(Icons.phone),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      TextFormField(
+                        obscureText: isHide,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                          ),
+                          suffixIcon: new GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isHide = !isHide;
+                              });
+                            },
+                            child: Icon(
+                                isHide ? Icons.visibility : Icons.visibility_off),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff0042C1),
+                            elevation: 0,
+                            textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                            ),
+                            padding: EdgeInsets.fromLTRB(24, 10, 24, 10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6)
+                            )
+                          ),
+                          child: Text("Register")
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      new GestureDetector(
+                        onTap: () {
+                          Navigator.pushAndRemoveUntil<dynamic>(
+                            context, 
+                            MaterialPageRoute<dynamic>(
+                              builder: (context) => Login()
+                            ), 
+                            (route) => false
+                          );
+                        },
+                        child: RichText(
+                          text: new TextSpan(
+                            style: new TextStyle(
+                              fontSize: 14,
+                              color: Colors.black
+                            ),
+                            children: <TextSpan>[
+                              new TextSpan(text: "Sudah Punya Akun? "),
+                              new TextSpan(
+                                text: "Masuk Disini", 
+                                style: TextStyle(
+                                  color: Color(0xff0042C1),
+                                  fontWeight: FontWeight.bold
+                                )
+                              ),
+                            ]
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        "KONTRAKIN",
+                        style: TextStyle(
+                          color: Color(0xff0042C1),
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ),
+          ],
+        ),
+      )
     );
   }
 }
