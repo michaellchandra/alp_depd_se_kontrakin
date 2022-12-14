608c470dddc48ebf70ab46b9605c0c72bad549f3
jverdianto <73513762+jverdianto@users.noreply.github.com>
2022-12-14 21:16:24 +0700

    splash screen added, painter adjusted

--- lib/views/pages/pages.dart
+++ lib/views/pages/pages.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 part 'login.dart';
 part 'register.dart';
 part 'pilihrole.dart';
+part 'splash.dart';
 
 class BluePainter extends CustomPainter {
   @override
@@ -32,9 +34,9 @@ class BluePainter extends CustomPainter {
 
     Path ovalPath2 = Path();
 
-    ovalPath2.moveTo(width * 0.5, 0);
+    ovalPath2.moveTo(width * 0.7, 0);
 
-    ovalPath2.quadraticBezierTo(width * 0.5, height * 0.2, width, height * 0);
+    ovalPath2.quadraticBezierTo(width * 0.7, height * 0.15, width, height * 0.15);
     
     ovalPath2.lineTo(width, 0);
 
@@ -77,9 +79,9 @@ class BluePainter2 extends CustomPainter {
 
     Path ovalPath2 = Path();
 
-    ovalPath2.moveTo(width * 0.3, 0);
+    ovalPath2.moveTo(width * 0.5, 0);
 
-    ovalPath2.quadraticBezierTo(width * 1.3, height * 0.5, width, 0);
+    ovalPath2.quadraticBezierTo(width * 0.5, height * 0.25, width, height * 0.23);
     
     ovalPath2.lineTo(width, 0);
 
@@ -87,6 +89,77 @@ class BluePainter2 extends CustomPainter {
 
     paint.color = const Color(0xff0042C1).withOpacity(0.3);
     canvas.drawPath(ovalPath2, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class BluePainter3 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Colors.white;
+    canvas.drawPath(mainBackground, paint);
+
+    Path ovalPath = Path();
+
+    ovalPath.moveTo(0, height * 0.8);
+
+    ovalPath.quadraticBezierTo(width * 0.25, height * 0.9, width * 0.3, height);
+    
+    ovalPath.lineTo(0, height);
+
+    ovalPath.close();
+
+    paint.color = const Color(0xff0042C1).withOpacity(0.3);
+    canvas.drawPath(ovalPath, paint);
+
+    Path ovalPath2 = Path();
+
+    ovalPath2.moveTo(width * 0.65, 0);
+
+    ovalPath2.quadraticBezierTo(width * 0.7, height * 0.15, width, height * 0.15);
+    
+    ovalPath2.lineTo(width, 0);
+
+    ovalPath2.close();
+
+    paint.color = const Color(0xff0042C1).withOpacity(0.3);
+    canvas.drawPath(ovalPath2, paint);
+
+    Path ovalPath3 = Path();
+
+    ovalPath3.moveTo(width * 0.3, 0);
+
+    ovalPath3.quadraticBezierTo(width * 0.3, height * 0.15, 0, height * 0.15);
+    
+    ovalPath3.lineTo(0, 0);
+
+    ovalPath3.close();
+
+    paint.color = const Color(0xff0042C1);
+    canvas.drawPath(ovalPath3, paint);
+
+    Path ovalPath4 = Path();
+
+    ovalPath4.moveTo(width * 0.8, height);
+
+    ovalPath4.quadraticBezierTo(width * 0.8, height * 0.9, width, height * 0.9);
+    
+    ovalPath4.lineTo(width, height);
+
+    ovalPath4.close();
+
+    paint.color = const Color(0xff0042C1);
+    canvas.drawPath(ovalPath4, paint);
   }
 
   @override
---
+++ lib/views/pages/splash.dart
@@ -0,0 +1,56 @@
+part of 'pages.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({ Key? key }) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.of(context).pushReplacement(
+        CupertinoPageRoute(builder: (ctx) => const Pilihrole())
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BluePainter3(),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "KONTRAKIN",
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                "solusi cepat nyari kontrakan",
+                style: TextStyle(
+                  fontSize: 16
+                ),
+              ),
+              SizedBox(height: 50),
+              SpinKitSpinningLines(
+                color: Color(0xff0042C1),
+                size: 50,
+              )
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
